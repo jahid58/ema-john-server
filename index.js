b303b11bd501c7e7ce74636631535e4f18860b65
Jahid Hasan <jahidjjaa@gmail.com>
2021-03-28 13:29:31 +0600

    asd

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+const MongoClient = require('mongodb').MongoClient;
+const port = 5000;
+require('dotenv').config()
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ntakd.mongodb.net/${process.env.DB_HOST}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+  const collection = client.db(process.env.DB_HOST).collection(process.env.DB_COLLECTION);
+  const ordersCollection = client.db(process.env.DB_HOST).collection('orders');
+  app.post('/addProduct',(req,res)=>{
+      const product = req.body
+      collection.insertMany(product)
+      .then(result=>{
+         res.send(result.insertedCount > 0)
+      })
+  })
+
+  app.post('/orderedProduct',(req,res)=>{
+    const ordered = req.body
+    ordersCollection.insertMany([ordered])
+    .then(result=>{
+       res.send(result.insertedCount > 0)
+    })
+})
+  app.get('/products',(req,res)=>{
+      collection.find({})
+      .toArray((err,documents)=>{
+          res.send(documents)
+      })
+  })
+  app.get('/product/:key',(req,res)=>{
+    collection.find({key:req.params.key})
+    .toArray((err,documents)=>{
+        res.send(documents[0])
+    })
+})
+app.post('/productsWithKeys',(req,res)=>{
+    const keys = req.body
+    collection.find({key:{$in:keys}})
+    .toArray((err,documents)=>{
+        res.send(documents)
+    })
+})
+})
+
+
+app.get('/',(req,res)=>{
+    res.send(process.env.DB_HOST)
+})
+
+app.listen(port)
